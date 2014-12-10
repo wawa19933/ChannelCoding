d6846e2032df261843ad06da3041a73dc4e88b66
wawa19933 <wawa19933@gmail.com>
2014-12-10 18:52:10 +0100

    Second edition

--- client.pl
+++ client.pl
@@ -8,7 +8,6 @@
 #		Packet structure:	| command | number | data | [CRC] |	#
 #						    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾	#
 #################################################################
-
 use strict;
 use warnings;
 
@@ -17,23 +16,25 @@ use File::Basename;				# Module for extracting file name from the path
 use MIME::Base64;				# Module for encoding binary data
 use Digest::CRC qw( crc64 crc32 crc ); # Module for CRC calculation
 use IO::Poll qw(POLLIN POLLOUT POLLHUP); # Module for ARQ realization: for data detection in a receiving buffer
+use Fcntl qw(SEEK_SET);
 
 $\ = "\n";						# Added as an invisible last element to the parameters passed to the print() function. *doc
 $| = 1;							# If nonzero, will flush the output buffer after every write() or print() function. Normally, it is set to 0. *doc
 				
-$argc = @ARGV;					# Checking the number of arguments
+my $argc = @ARGV;				# Checking the number of arguments
 if ($argc < 1) {
 	HELP_MESSAGE();
 	exit;
 }
 
 my $filePath = shift; 	     			# Taking the file's path from the FIRST argument
-my $hostAddr = shift or 'localhost';	# Taking the server's address from the SECOND argument
+my $hostAddr = shift || 'localhost';	# Taking the server's address from the SECOND argument
 my $port = '8849';						# Variable for port number defenition
 my $filename = fileparse ( $filePath );	# Extracting the name of the file
-my ( $buffer, $counter, $sz = 1024, @arq ); # Some global variables
-
-print "Going to send $filename to $hostAddr...";
+my ( $buffer, $counter, @arq ); 	# Some global variables
+my $sz = 1024;
+my $fileSize = -s $filePath;
+print "Going to send $filename to $hostAddr";
 
 #	Opening the specified file in BINARY form for reading only
 open FILE, '<', $filePath or die "Can not open the file!!! $filePath: $!";
@@ -50,38 +51,26 @@ my $socket = IO::Socket::INET->new(
 
 # 1) --	Sending the file name to server	#################################
 $counter = 0;
-my @msg = ( 'NAME', $counter, $filename );
+my @msg = ( 'NAME', $counter, $filename, $fileSize );
 $socket->send ( join ';', @msg ) or die "Sending error: $!";
 
 # 2) --	Sending the whole file by parts of $sz Bytes size
-my $check = IO::Poll->new;					# Creating an object for poll() call
-$check->mask ( $socket => POLLIN );			# and watching for incoming data
- 
-while ( read FILE, $data, $sz > 0 ) {							# This loop performs reading 
-	print sendData ( $data ) . " bytes are sent to $hostAddr";
-	
-	if ( $check->poll (15) ) {
-		my $arqMsg;
-		$socket->recv ( $arqMsg, 255 );
-		my @packet = split ';', $arqMsg;
-	}
+while ( read FILE, $buffer, $sz > 0 ) {						# This loop performs reading the file untill the end of it
+	my $res = sendData ( $buffer );
+	print " $res bytes are sent to $hostAddr";				# sending the prepared data
 }
 
-$counter++;
-# $message = "$counter;EXIT;";
-$socket->send( $message ) or die "Sending error: $! $endl";
-
-print "\n";
+print "\nThe end\n";
 
 close FILE;       # Closing the file
 
 sub HELP_MESSAGE {
-	print "\nUsage: client.pl [host] [file] \n\n";
+	print "\nUsage: client.pl [file] [host] \n";
 }
 
 sub sendData {
-	my $rawData = shift;					# First parameter of the function
-	my $encoded, $hash;
+	my $rawData = shift;					# Parameter of the function is data from file
+	my ( $encoded, $hash ); 
 	
 	$counter ++;
 	$hash = crc32 ( $rawData );
@@ -93,5 +82,27 @@ sub sendData {
 }
 
 sub repeatSending {
+	my ( $cmd, $num, $data ) = @_;
+	my $pos = tell FILE;
+	
+	if ( seek FILE, $num * $sz, SEEK_SET ) {
+		if ( read FILE, $buffer, $sz > 0 ) {
+			print sendData ( $buffer ) . " bytes of $num ARQ are re-sent to $hostAddr";
+		}
+	}
+	
+	seek FILE, $pos, SEEK_SET;
+}
 
+sub checkForPackets {
+	my $check = IO::Poll->new;					# Creating an object for poll() call
+	$check->mask ( $socket => POLLIN );			# and watching for incoming data
+
+	if ( $check->poll (15) ) {										# checking for the incoming ARQ in receiving 
+		my $arqMsg;
+		$socket->recv ( $arqMsg, 255 );
+		my @packet = split ';', $arqMsg;
+		
+		repeatSending (@packet);
+	}
 }
--- server.pl
+++ server.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
 
@@ -10,36 +9,39 @@ use v5.14;
 $| = 1;							# Flushing to SOCKET after each write
 $\ = "\n";						# Added as an invisible last element to the parameters 
 								# passed to the print() function. *doc
-# my $endl = "\n";
-my $ask = shift or 0;
+my $ask = shift || 0;
 my $port = '8849';
-my ( $data, $message, $counter, $peerAddress, $peerName );
+my ( $buffer, $message, $counter, $peerAddress, $peerName, $fileSize, $fileName, %fileParts, @arq );
 
 my $socket = IO::Socket::INET->new(
 				LocalPort => $port,
 				Proto => 'udp',
 				Type => SOCK_DGRAM
-) or die "Could not start server!!! : $!\n";
+	) or die "Could not start server!!! : $!";
 
 ##		Starting the receiving loop that is controlled by 	##
 ##		commands from the client via Switch-like statment	##
 while () {
-	my ( $senderIP, $senderName ) = getIP $socket->recv ( $message, 2048 );		# Waits untill the packet is received and read socket's buffer to $message
+	my ( $senderIP, $senderName ) = getIP ($socket->recv ( $message, 2048 )); # Waits untill the packet is received and read socket's buffer to $message
 	print length ( $message ) . " bytes from $senderName ($senderIP)";
 	
-	my @parts = split ';', $message;		# Splitting the packet into array: ( command, numberOfPacket, Data )
-	my $cmd = shift @parts;
+	my @packet = split ';', $message;			# Splitting the packet into array: ( command, numberOfPacket, Data )
+	my $cmd = shift @packet;					# Taking command field from packet
 	
 	 given ($cmd) {
 		when ('NAME') {
-			
-			print $data . $endl;
-			
+			# $fileSize = pop @packet;
+			# $fileName = pop @packet;
+			# open FILE, '>', $fileName || die "Can not open file $fileName for writting: $!";
+			openFile ( @packet );
+			print "Receiving $fileName ...";
 		}
 		when ('DATA') {
-			print decode_base64( $data ) . $endl . $endl;
+			receiveData ( @packet );
 		}
-		
+		when ('FINISH') {
+			$counter = 0;
+		}
 		when ('EXIT') {
 			exit;
 		}
@@ -50,35 +52,50 @@ close $socket;
 
 # ============= Functions ===================
 sub receiveData {
-	my ( $num, $encoded ) = @_; 			# Packet number and encoded data from socket as parameters
-	my ( $rawData, $ecncoded, $output );
+	my ( $num, $encoded, $hash ) = @_; 				# Packet number and encoded data from socket as parameters
+	my ( $rawData, $crc );
 	$counter ++;
 	
-	if ( $counter != $num ) {
-		
-	} 
+	# if ( $counter != $num ) {
+		# @arq = $counter;
+	# } 
 	
-	
+	$rawData = decode_base64 ( $encoded );
+	$crc = crc32 ( $rawData );
+	if ( $crc == $hash ) {
+		$fileParts{ $num } = $rawData;
+	}
+	else {
+		print "$num - Checksums differ!"
+		push @arq, $num;
+	}
 }
 
 sub openFile {
-	my $fileName = shift;
+	my $fileSize = pop @_;
+	my $fileName = pop @_;
 	if ( $ask ) {
 		$_ = "";
 		print "Enter the file name to save: ";
 		$fileName = <>;	
 	}
-	open FILE, '>', $fileName or die "Can not write the file $fileName: $!";
+	open FILE, '>', $fileName || die "Can not write the file $fileName: $!";
+}
+
+sub checkBuffer {
+	foreach $k ( keys %fileParts ) {
+		
+	}
 }
 
 sub getIP {
-	my ( $port, $iaddr ) = sockaddr_in ( shift );
-	
+	my ( $port, $iaddr ) = sockaddr_in ( shift @_ );
 	my $peerAddress = inet_ntoa( $iaddr );
 	my $peerName = gethostbyaddr ( $iaddr, AF_INET );
 	
+	# print "$peerAddress, $peerName";
 	if ( wantarray() ) {				# List context
-		return [ $peerAddress, $peerHost ];
+		return ( $peerAddress, $peerName );
 	} 
 	elsif ( defined wantarray() ) {		# Scalar context
 		return $peerAddress;
