5e2315c23056f2832001ee9b86d3995aaeec2a0e
Sergio <wawa19933@gmail.com>
2014-12-14 20:00:41 +0100

    Comments are added

--- server.pl
+++ server.pl
@@ -2,117 +2,107 @@
 use strict;
 use warnings;
 
-use IO::Socket::INET;
-use MIME::Base64;
-# use Digest::CRC qw( crc64 crc32 crc ); # Module for CRC calculation
-use v5.14;
+use IO::Socket::INET;         	# Module that adds the socket support
+use MIME::Base64;				# Module for encoding binary data
+use v5.12;						# For support of given-when statement
 
-$| = 1;							# Flushing to SOCKET after each write
-$\ = "\n";						# Added as an invisible last element to the parameters passed to the print() function. *doc
-my $ask = shift || 0;
-my $port = '8849';
-my ( $buffer, $message, $counter, $peerAddress, $peerName, $fileSize, $fileName, %fileParts, @arq );
+$| = 1;				# Flushing to SOCKET after each write
+$\ = "\n";			# Added as an invisible last element to the parameters passed to the print() function. *doc
 
-my $socket = IO::Socket::INET->new(
-				LocalPort => $port,
+my $port = '8849';				# Declaring of port number for socket
+my ( $buffer, $message, $counter, $peerAddress, $peerName, $fileSize, $fileName, $packetsCount, %fileParts, @arq ); # Global variables declaration
+
+my $socket = IO::Socket::INET->new(				# Initialization of the socket
+				LocalPort => $port,		
 				Proto => 'udp',
 				Type => SOCK_DGRAM
 	) or die "Could not start server!!! : $!";
 
 ##		Starting the receiving loop that is controlled by 	##
 ##		commands from the client via Switch-like statment	##
-while () {
+
+while () {									  		# Endless working loop
 	my ( $senderIP, $senderName ) = getIP ($socket->recv ( $message, 2048 )); # Waits untill the packet is received and read socket's buffer to $message
-	print length ( $message ) . " bytes from $senderName ($senderIP)";
+	print length ( $message ) . " bytes from $senderName ($senderIP)";	  
+	my @packet = split ';', $message;				# Splitting the packet into array: ( command, numberOfPacket, Data )
+	my $cmd = shift @packet;						# Taking command field from packet
 	
-	my @packet = split ';', $message;			# Splitting the packet into array: ( command, numberOfPacket, Data )
-	my $cmd = shift @packet;					# Taking command field from packet
-	
-	 given ($cmd) {
-		when ('NAME') {
-			# $fileSize = pop @packet;
-			# $fileName = pop @packet;
-			# open FILE, '>', $fileName || die "Can not open file $fileName for writting: $!";
-			openFile ( @packet );
+	 given ($cmd) {							# Switching of the received commands
+		when ('NAME') {						# Receiving the information about the file
+			$fileSize = pop @packet;		# Taking file size from the packet
+			$fileName = pop @packet;		# Taking file name from the packet
+			$packetsCount = pop @packet;	# Taking number of the packets to be sent
+			if ( tell ( FILE ) != -1 ) {		# Check whether file is opened		
+			    close FILE;					# If it is open -> close it
+			}
+			open FILE, '>', $fileName || die "Can not open file $fileName for writting: $!"; # Open file for writing 
 			print "Receiving $fileName ...";
 		}
-		when ('DATA') {
+		when ('DATA') {						# Receiving the parts of the file
 			receiveData ( @packet );
 		}
-		when ('CHECK') {
-			print scalar @arq;
-			prepareARQ ();
-			processARQ ();
+		when ('CHECK') {					# ARQ realization
+			prepareARQ ();					# Check for lost packets
+			processARQ ();					# Request for repeating of the lost parts
 		}
 		when ('FINISH') {
-			$counter = 0;
+			$counter = 0;					# 
 		}
-		when ('EXIT') {
+		when ('EXIT') {						# Close the program
 			exit;
 		}
 	 }
 }
 
-close $socket;			# Closing the socket
-close FILE;				# Closing the file
+close $socket;				# Closing the socket
+close FILE;					# Closing the file
 
-# ============= Functions ===================
+# ============= Functions ===================#
+
 sub receiveData {
 	my ( $num, $encoded ) = @_; 				# Packet number and encoded data from socket as parameters
-	my $rawData;
-	$counter ++;
+	my $rawData;								# Variable for decoded data
 	
-	$rawData = decode_base64 ( $encoded );			# Decoding file part from Base64
+	$rawData = decode_base64 ( $encoded );		# Decoding file part from Base64
+	%fileParts{ $num } = $rawData;				# Put file's data into associative array with packet's number as a key
 }
 
-sub openFile {
-	$fileSize = pop @_;
-	$fileName = pop @_;
-	if ( $ask ) {
-		$_ = "";
-		print "Enter the file name to save: ";
-		$fileName = <>;	
-	}
-	open FILE, '>', $fileName || die "Can not write the file $fileName: $!";
-}
-
-sub prepareARQ {
-	my $count = 0;
-	my $packetsCount = $fileSize / 1024;
-	for ( my $i = 0; $i < $packetsCount; $i++ ) {
-		my $ok = 0;
-		foreach my $k ( keys %fileParts ) {
-			if ( $i == $k ) {
-				$ok = 1;
-				break;
+sub prepareARQ {								# Checking for the lost packets
+	my $count = 0;								# Just a counter	
+	for ( my $i = 1; $i <= $packetsCount; $i++ ) {		# Loop for checking the lost packets
+		my $ok = 0;								# Flag for marking found parts  
+		foreach my $k ( keys %fileParts ) {		# Going through the all received parts' numbers
+			if ( $i == $k ) {					# Find out the current order number of the part	 
+				$ok = 1;						# If statement is true - mark as a found				
+				break;							# Stop the loop of searching	
 			}
 		}
-		if ( $ok ) {
+		if ( $ok ) {							# If previous actions are successfull - go forward
 			continue;
 		}
-		else {
-			push @arq, $i;
+		else {									# If not ->
+			push @arq, $i;						# 	append them to the ARQ queue
 		}
 	}
 }
 
-sub processARQ {
-	my $count = @arq;
-	foreach my $num ( @arq ) {
-		my @msg = ( 'ARQ', $num, $count-- );
-		$socket->send ( join ';', @msg );
+sub processARQ {								# Request for repeating of the lost parts 
+	my $count = @arq;							# Counting the number of packets necessary to be repeated
+	foreach my $num ( @arq ) {					# Going through all the ARQs  
+		my @msg = ( 'ARQ', $num, $count-- );	# Forming the packet
+		$socket->send ( join ';', @msg );		# Send the request for lost packet
 	}
 }
 
-sub getIP {
-	my ( $port, $iaddr ) = sockaddr_in ( shift @_ );
-	my $peerAddress = inet_ntoa( $iaddr );
-	my $peerName = gethostbyaddr ( $iaddr, AF_INET );
+sub getIP {										# Utilit function for taking the sender's address
+	my ( $port, $iaddr ) = sockaddr_in ( shift @_ );	# 
+	my $peerAddress = inet_ntoa( $iaddr );				#
+	my $peerName = gethostbyaddr ( $iaddr, AF_INET );	#
 	
-	if ( wantarray() ) {				# List context
-		return ( $peerAddress, $peerName );
+	if ( wantarray() ) {						# List context
+		return ( $peerAddress, $peerName );		#
 	} 
-	elsif ( defined wantarray() ) {		# Scalar context
+	elsif ( defined wantarray() ) {				# Scalar context
 		return $peerAddress;
 	} 
 }
